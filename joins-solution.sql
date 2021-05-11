54ea497a9a3aff14fcaf974cca41db3d1a304924
Chloe Everson <chloeliz93@gmail.com>
2021-05-11 11:29:31 -0500

    warehouse -> product find

--- joins-solution.sql
+++ joins-solution.sql
@@ -7,3 +7,22 @@ SELECT * FROM orders o
 JOIN line_items l ON l.order_id = o.id
 JOIN products p ON p.id = l.product_id;
 
+--3. Which warehouses have cheetos?
+delta
+
+SELECT w.warehouse, p.description  FROM products p
+JOIN warehouse_product wp ON wp.product_id = p.id
+JOIN warehouse w ON w.id = wp.warehouse_id
+WHERE p.description = 'cheetos';
+
+
+--4. Which warehouses have diet pepsi?
+alpha, delta, & gamma
+
+SELECT w.warehouse, p.description  FROM products p
+JOIN warehouse_product wp ON wp.product_id = p.id
+JOIN warehouse w ON w.id = wp.warehouse_id
+WHERE p.description = 'diet pepsi';
+
+
+
